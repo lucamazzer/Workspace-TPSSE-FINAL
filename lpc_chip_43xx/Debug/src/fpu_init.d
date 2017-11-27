14548ac5c1d1ea7b69f4352149d66aed1f5124b7
lucamazzer <lucamazzer92@gmail.com>
2017-11-27 19:22:46 -0300

    cambio algo del debug, no se que es

--- lpc_chip_43xx/Debug/src/fpu_init.d
+++ lpc_chip_43xx/Debug/src/fpu_init.d
@@ -1,28 +1,28 @@
 src/fpu_init.o src/fpu_init.d: ../src/fpu_init.c \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/sys_config.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/cmsis.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/lpc_types.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/sys_config.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/cmsis_43xx.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cm4.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cmInstr.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cmFunc.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cm4_simd.h
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/sys_config.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/cmsis.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/lpc_types.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/sys_config.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/cmsis_43xx.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cm4.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cmInstr.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cmFunc.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cm4_simd.h
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/sys_config.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/sys_config.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/cmsis.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/cmsis.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/lpc_types.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/lpc_types.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/sys_config.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/sys_config.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/cmsis_43xx.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/cmsis_43xx.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cm4.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cm4.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cmInstr.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cmInstr.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cmFunc.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cmFunc.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/core_cm4_simd.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/core_cm4_simd.h:
--- lpc_chip_43xx/Debug/src/ring_buffer.d
+++ lpc_chip_43xx/Debug/src/ring_buffer.d
@@ -1,7 +1,7 @@
 src/ring_buffer.o src/ring_buffer.d: ../src/ring_buffer.c \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/ring_buffer.h \
- C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/lpc_types.h
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/ring_buffer.h \
+ C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/lpc_types.h
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/ring_buffer.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/ring_buffer.h:
 
-C:\Users\luca10\Desktop\pwm\lpc_chip_43xx\inc/lpc_types.h:
+C:\Users\luca10\Desktop\EMbebidos\Workspace-TPSSE-FINAL\lpc_chip_43xx\inc/lpc_types.h:
